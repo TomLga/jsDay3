cde92cbab93bf68c73f05e24b55f5e7d95db7878
TomLga <semitallbox@gmail.com>
2023-05-17 09:53:34 +0200

    Q 2

--- script/code.js
+++ script/code.js
@@ -3,3 +3,8 @@ let words="jeff"
 console.log(typeof (words))
 
 
+//2.Reassign the value to a number and check the type again. Did it change?
+
+let jeff = name;
+jeff= 56
+console.log(typeof (jeff))
